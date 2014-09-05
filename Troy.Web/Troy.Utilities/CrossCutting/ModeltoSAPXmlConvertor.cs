0921e734ab59627425be2129151b49ece1748fbc
Jana <aa@gmail.com>
2014-09-05 17:06:52 +0530

    ModeltoSAPXmlConvertor class

---
+++ Troy.Web/Troy.Utilities/CrossCutting/ModeltoSAPXmlConvertor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Troy.Utilities.CrossCutting
+{
+    public class ModeltoSAPXmlConvertor
+    {
+
+        public void ConvertModelToXMLString()
+        {
+            Guid g = Guid.NewGuid();
+            string random = g.ToString();
+        }
+    }
+}
