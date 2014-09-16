c9708cbbf895e72ddf6fc9c148acdce56d99c049
sakthiganeshTroy <sakthiganesh.c@hotmail.com>
2014-09-17 00:36:52 +0530

    Updated the application layout and Menu styles. Purchase master screen has been updated with new standard styles.

--- Troy.Web/Troy.Web/App_Start/BundleConfig.cs
+++ Troy.Web/Troy.Web/App_Start/BundleConfig.cs
@@ -10,101 +10,75 @@ namespace Troy.Web
         {
             bundles.IgnoreList.Clear();
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                         "~/Scripts/jquery-{version}.js"));
+            #region Css files
+            // Icons 
+            bundles.Add(new StyleBundle("~/Content/themes/icons").Include(
+                        "~/Content/themes/TroyPlus/css/icons.css"));
+            // jQueryUI 
+            bundles.Add(new StyleBundle("~/Content/themes/jqueryUI").Include(
+                            "~/Content/themes/TroyPlus/css/sprflat-theme/jquery.ui.all.css"));
+            // Bootstrap stylesheets (included template modifications) 
+            bundles.Add(new StyleBundle("~/Content/themes/bootstrap").Include(
+                            "~/Content/themes/TroyPlus/css/bootstrap.css"));
+            // Plugins stylesheets (all plugin custom css) 
+            bundles.Add(new StyleBundle("~/Content/themes/plugins").Include(
+                            "~/Content/themes/TroyPlus/css/plugins.css"));
+            // Main stylesheets (template main css file) 
+            bundles.Add(new StyleBundle("~/Content/themes/mainStyles").Include(
+                            "~/Content/themes/TroyPlus/css/main.css"));
+            // Custom stylesheets ( Put your own changes here ) 
+            bundles.Add(new StyleBundle("~/Content/themes/custom").Include(
+                            "~/Content/themes/TroyPlus/css/custom.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+            #endregion
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js"));
+            #region Javascripts
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+            // Load pace first -->
+            bundles.Add(new ScriptBundle("~/bundles/script/jPace").Include("~/Content/themes/TroyPlus/plugins/core/pace/pace.min.js"));
+            // Important javascript libs(put in all pages) -->
+            bundles.Add(new ScriptBundle("~/bundles/script/jLibs").Include("~/Content/themes/TroyPlus/js/jquery-2.1.1.min.js"
+                , "~/Content/themes/TroyPlus/js/jquery-ui-js.js",
+                "~/Content/themes/TroyPlus/js/libs/jquery-ui-1.10.4.min.js"
+                ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquerydata").Include(
-                        "~/Scripts/jquery.dataTables.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstraptable").Include(
-                        "~/Scripts/dataTables.bootstrap.js"));
-
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/Site").Include(
-                                               "~/Content/Site.css"));
-
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                                               "~/Content/bootstrap-switch.css",
-                                               "~/Content/bootstrap.css",
-                                               "~/Content/responsive.css",
-                                               "~/Content/custom.css"));
-
-            bundles.Add(new StyleBundle("~/Content/jquerycustom").Include("~/Content/jquery-ui-1.10.4.custom.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+            // Bootstrap plugins -->
+            bundles.Add(new ScriptBundle("~/bundles/script/bootstrap").Include("~/Content/themes/TroyPlus/js/bootstrap/bootstrap.js"));
+            // Core plugins ( not remove ever) -->
+            // Handle responsive view functions -->
+            bundles.Add(new ScriptBundle("~/bundles/script/jRespond").Include("~/Content/themes/TroyPlus/js/jRespond.min.js"));
+            // Custom scroll for sidebars,tables and etc. -->
+            bundles.Add(new ScriptBundle("~/bundles/script/jSlimScrollBar").Include("~/Content/themes/TroyPlus/plugins/core/slimscroll/jquery.slimscroll.min.js"
+                , "~/Content/themes/TroyPlus/plugins/core/slimscroll/jquery.slimscroll.horizontal.min.js"));
+            // Resize text area in most pages -->
+            bundles.Add(new ScriptBundle("~/bundles/script/jAutoSize").Include("~/Content/themes/TroyPlus/plugins/forms/autosize/jquery.autosize.js"));
+            // Proivde quick search for many widgets -->
+            bundles.Add(new ScriptBundle("~/bundles/script/jSearch").Include("~/Content/themes/TroyPlus/plugins/core/quicksearch/jquery.quicksearch.js"));
+            // Bootbox confirm dialog for reset postion on panels -->
+            bundles.Add(new ScriptBundle("~/bundles/script/jBootbox").Include("~/Content/themes/TroyPlus/plugins/ui/bootbox/bootbox.js"));
 
 
-            bundles.Add(new StyleBundle("~/bundles/themes/TroyPlus").Include(
-                "~/Content/themes/TroyPlus/css/less/sidebar.less",
-                "~/Content/themes/TroyPlus/css/less/bootstrap.less",
-                "~/Content/themes/TroyPlus/css/less/custom-variables.less",
-                "~/Content/themes/TroyPlus/css/less/app.less",
-                "~/Content/themes/TroyPlus/css/less/plugins/highlight.less",
-                "~/Content/themes/TroyPlus/css/bootstrap.css",
-                "~/Content/themes/TroyPlus/css/plugins.css",
-                "~/Content/themes/TroyPlus/css/main.css",
-                "~/Content/themes/TroyPlus/css/custom.css",
-                "~/Content/themes/TroyPlus/css/icons.css",
-                "~/Content/themes/TroyPlus/css/sprflat-theme/jquery.ui.all.css",
-                "~/Content/themes/TroyPlus/plugins/misc/highlight/styles/"));
+            // Common scripts for master data pages.
+            bundles.Add(new ScriptBundle("~/bundles/script/commonScripts").Include(
+                "~/Content/themes/TroyPlus/plugins/core/moment/moment.min.js",
+                "~/Content/themes/TroyPlus/plugins/charts/sparklines/jquery.sparkline.js",
+                "~/Content/themes/TroyPlus/plugins/charts/pie-chart/jquery.easy-pie-chart.js",
+                "~/Content/themes/TroyPlus/plugins/forms/icheck/jquery.icheck.js",
+                "~/Content/themes/TroyPlus/plugins/forms/tags/jquery.tagsinput.min.js",
+                "~/Content/themes/TroyPlus/plugins/forms/tinymce/tinymce.min.js",
+                "~/Content/themes/TroyPlus/plugins/tables/datatables/jquery.dataTables.min.js",
+                "~/Content/themes/TroyPlus/plugins/tables/datatables/jquery.dataTablesBS3.js",
+                "~/Content/themes/TroyPlus/plugins/tables/datatables/tabletools/ZeroClipboard.js",
+                "~/Content/themes/TroyPlus/plugins/tables/datatables/tabletools/TableTools.js",
+                "~/Content/themes/TroyPlus/plugins/misc/highlight/highlight.pack.js",
+                "~/Content/themes/TroyPlus/plugins/misc/countTo/jquery.countTo.js",
+                "~/Content/themes/TroyPlus/js/jquery.sprFlat.js",
+                "~/Content/themes/TroyPlus/js/app.js",
+                "~/Content/themes/TroyPlus/js/pages/data-tables.js",
+                "~/Content/themes/TroyPlus/js/libs/jQuery-placeholder.js"
+                ));
 
-            // Wrapbootstrap
-            bundles.Add(new ScriptBundle("~/bundles/scripts/TroyPlus").
-                Include(
-            "~/Content/themes/TroyPlus/plugins/core/quicksearch/jquery.quicksearch.js",
-            "~/Content/themes/TroyPlus/plugins/misc/highlight/highlight.pack.js",
-            "~/Content/themes/TroyPlus/js/html5shiv.js",
-            "~/Content/themes/TroyPlus/js/bootstrap/bootstrap.js",
-            "~/Content/themes/TroyPlus/js/jquery.sprFlat.js",
-            "~/Content/themes/TroyPlus/js/jRespond.min.js",
-            "~/Content/themes/TroyPlus/js/bootstrap/bootstrap.js",
-            "~/Content/themes/TroyPlus/plugins/core/moment/moment.min.js",
-             "~/Content/themes/TroyPlus/js/app.js",
-             "~/Content/themes/TroyPlus/plugins/core/pace/pace.min.js",
-             "~/Content/themes/TroyPlus/plugins/core/slimscroll/jquery.slimscroll.min.js",
-             "~/Content/themes/TroyPlus/plugins/core/slimscroll/jquery.slimscroll.horizontal.min.js",
-             "~/Content/themes/TroyPlus/plugins/forms/autosize/jquery.autosize.js",
-             "~/Content/themes/TroyPlus/plugins/ui/bootbox/bootbox.js",
-             "~/Content/themes/TroyPlus/plugins/misc/countTo/jquery.countTo.js",
-             "~/Content/themes/TroyPlus/plugins/forms/icheck/jquery.icheck.js",
-             "~/Content/themes/TroyPlus/js/jRespond.min.js",
-             "~/Content/themes/TroyPlus/plugins/forms/tinymce/tinymce.min.js",
-             "~/Content/themes/TroyPlus/plugins/forms/tags/jquery.tagsinput.min.js",
-             "~/Content/themes/TroyPlus/plugins/core/moment/moment.min.js",
-             "~/Content/themes/TroyPlus/plugins/forms/tags/jquery.tagsinput.min.js",
-             "~/Content/themes/TroyPlus/plugins/tables/datatables/jquery.dataTables.min.js",
-            "~/Content/themes/TroyPlus/plugins/tables/datatables/jquery.dataTablesBS3.js",
-            "~/Content/themes/TroyPlus/plugins/tables/datatables/tabletools/ZeroClipboard.js",
-            "~/Content/themes/TroyPlus/plugins/tables/datatables/tabletools/TableTools.js"
-             ));
+            #endregion
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
             //BundleTable.EnableOptimizations = true;
