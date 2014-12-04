2aa6bd941109c25fbf0ba6c77488a087b3fd0f9e
prakashtroy <prakash.shahi12@gmail.com>
2014-12-04 10:50:31 +0530

    commit salesdelivery

---
+++ Troy.Web/Troy.Model/SalesDelivery/SalesDelivery.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Troy.Model.SalesDelivery
+{
+    [Table("tblSalesDelivery")]
+    public class SalesDelivery
+    {
+
+        [Key]
+        [Required]
+        public int Sales_Delivery_Id { get; set; }
+
+        [Required]
+        public int BaseDocId { get; set; }
+
+        [Required]
+        public int TargetDocId { get; set; }
+
+        [Required]
+        public int Sales_Order_Id { get; set; }
+
+
+        [Required]
+        public int Customer { get; set; }
+
+        [Required]
+
+        public string Reference_Number { get; set; }
+
+        [Required]
+        public string Doc_Status { get; set; }
+
+        [Required]
+        public DateTime Posting_Date { get; set; }
+
+        [Required]
+        public DateTime Delivery_Date { get; set; }
+
+        [Required]
+        public DateTime Document_Date { get; set; }
+
+        [Required]
+        public int Branch { get; set; }
+
+        [Required]
+        public Decimal TotalBefDocDisc { get; set; }
+
+        [Required]
+        public Decimal DocDiscAmt { get; set; }
+
+        [Required]
+        public Decimal TaxAmt { get; set; }
+
+        [Required]
+        public Decimal TotalSlsDlvryAmt { get; set; }
+
+        [Required]
+        public String Remarks { get; set; }
+
+        [Required]
+        public int Created_User_Id { get; set; }
+
+        [Required]
+        public int Created_Branc_Id { get; set; }
+
+        [Required]
+        public DateTime Created_Date { get; set; }
+
+        [Required]
+        public int Modified_User_Id { get; set; }
+
+        [Required]
+        public int Modified_Branc_Id { get; set; }
+
+        [Required]
+        public DateTime Modified_Date { get; set; }
+
+    }
+}
